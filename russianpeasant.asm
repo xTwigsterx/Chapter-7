a73f14ddc38c142c4a37e8f4f43438fa113a7733
Zachariah Alber <zachariah.man.of.god@gmail.com>
2015-10-19 09:17:45 -0400

    rough draft

--- russianpeasant.asm
+++ russianpeasant.asm
@@ -1,77 +1,35 @@
 ; Zachariah Alber
 ;russian peasant multiplication algorithm
-;most of this is based on a file online
-;not my final draft
-    section .text
 
-    global mult
+386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
 
-mult:
-    push rbp
-    mov  rbp, rsp
-    sub  rsp, 8     ;allocate m
-
-    mov  qword [rbp-8], 0   ; m = 0;
-.while:
-    test rdi, rdi   ; x == 0?
-    je .done
-    test rsi, rsi   ; y == 0?
-    je .done
-
-    test rdi, 0x01  ; x is odd?
-    jz .shifts
-    add  [rbp-8], rsi   ; m += y;
-.shifts:
-    shr  rdi, 1     ; x >>= 1;
-    shl  rsi, 1     ; y <<= 1;
-    jmp  .while
-
-.done:
-    mov  rax, [rbp-8]
-    ;mov  rsp, rbp
-    ;pop  rbp
-    leave
-    ret
+.data
+numX dword 15
+numY dword 24
 
 
-    global mult128
+.code
+main PROC
 
-; Locate the parameters:
-%define Pupper64 rdi
-%define Plower64 rsi
-%define x64 rdx
-%define y64 rcx
+mov eax, numX
+mov ebx, numY
 
-mult128:
-    push rbp
-    mov  rbp, rsp
-    sub  rsp, 8     ;allocate y128
-%define Py128 rbp-8
+L1:
+	mult eax,2 ; 
+	div  ebx,2 ;
+	test ebx, ebx, 00000001 ;
+	jz Redo
+	loop L1
+	
+Redo:
+	jmp L1
 
-    mov  qword [Py128], 0   ; y128 = 0;
-    mov  qword [Pupper64], 0   ; *upper64 = 0;
-    mov  qword [Plower64], 0   ; *lower64 = 0;
-.while:
-    test x64, x64       ; x64 == 0?
-    jz .done            ; x64 is 0?
+done:
+	invoke ExitProcess,0
+main endp
+end main
 
-    test x64, 0x01      ; is x64 odd?
-    jz .shifts          ; x64 not odd?
-
-    mov  rax, [Py128]   ; (can't add directly)
-    add  [Pupper64], rax
-    add  [Plower64], y64
-    jnc  .shifts        ; no carry from lower64 to upper64?
-    inc  qword [Pupper64]       ; carry from lower64 to upper64!
-
-.shifts:
-    shr  x64, 1         ; x64 >>= 1;
-    shl  qword[Py128], 1; y128 <<= 1;
-    shl  y64, 1         ; y64 <<= 1;
-    jnc  .while         ; no carry from y64 to y128?
-    inc  qword[Py128]           ; carry from y64 to y128!
-    jmp  .while
-
-.done:
-    leave
-    ret
+ 
